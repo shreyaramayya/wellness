a85134b2e97ba1db98ee3e0bbc4b35af7c4b7927
Shreya Ramayya <66265148+shreyaramayya@users.noreply.github.com>
2023-09-20 07:27:27 -0700

    modifying exercise logger

--- exerciselog.py
+++ exerciselog.py
@@ -5,23 +5,16 @@ import csv
 today = dt.date.today()
 d1 = today.strftime("%d/%m/%y")
 
-# collect information on the work day
+# collect information on workouts
 rot = input("What rotation are you on? ")
-start = input("When did you start work? ")
-end = input("When did you leave work? ")
+type = input("What type of exercise did you do? ")
+time = input("How long did you work out? ")
+cals = input("How many calories did you burn? ")
+dist = input("What distance did you cover today? ")
 notes = input("What happened today at work? ")
 
-# calculate hours worked
-start_dt = dt.datetime.strptime(start, '%d/%m/%y %H:%M')
-end_dt = dt.datetime.strptime(end, '%d/%m/%y %H:%M')
-diff = (end_dt - start_dt)
-hours = diff.total_seconds()/3600
-
-#print summary
-print ("Today's Summary", d1, rot, hours, notes)
-
-#write to hour log
-with open('WorkHours.csv', 'a', newline='') as file:
-    fieldnames = [Date, Type, Time_In_Mins, Cals, Distance, Notes]
+#write to exercise log
+with open('Exercise.csv', 'a', newline='') as file:
+    fieldnames = ['Date', 'Type', 'Time_In_Mins', 'Cals', 'Distance', 'Notes']
     writer = csv.DictWriter(file, fieldnames=fieldnames)
-    writer.writerow({'Date': d1, 'Rotation': rot, 'Hours': hours, 'Notes': notes})
+    writer.writerow({'Date': d1, 'Type': type, 'Time_In_Mins': time, 'Cals': cals, 'Distance': dist, 'Notes': notes})
